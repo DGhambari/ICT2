e8c0927942b99260e732d581dae3034e2b981ed1
DGhambari <darrenghambari@yahoo.com>
2022-06-21 22:48:13 +0100

    Exercise 2 completed.

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -1,36 +1,31 @@
-import React from "react";
-import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const Demo = () => {
   return (
     <>
-			<h1>HDip Computer Science - Modules table</h1>
-			<table className='table table-bordered'>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>No lectures</th>
-						<th>No practicals</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>DevOps</td>
-						<td>2</td>
-						<td>2</td>
-					</tr>
-					<tr>
-						<td>Enterprise Web Dev</td>
-						<td>3</td>
-						<td>2</td>
-					</tr>
-				</tbody>
-			</table>
-
-			{/* <ul>
-        <li>Erlang</li>
-        <li>Clojure</li>
-      </ul> */}
+      <h1>HDip Computer Science - Modules table</h1>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>DevOps</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>Enterprise Web Dev</td>
+            <td>3</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -1,10 +1,37 @@
-import React from "react";
-import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
-
+import React from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const Demo = () => {
+  const modules = [
+    { name: 'DevOps', noLectures: '2', noPracticals: '2' },
+    { name: 'Enterprise Web Dev', noLectures: '2', noPracticals: '2' },
+  ];
+  const header = 'HDip Computer Science';
   return (
-      <h1>TODO</h1>
+    <>
+      <h1>{`${header}  - Modules table`}</h1>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{modules[0].name}</td>
+            <td>{modules[0].noLectures}</td>
+            <td>{modules[0].noPracticals}</td>
+          </tr>
+          <tr>
+            <td>{modules[1].name}</td>
+            <td>{modules[1].noLectures}</td>
+            <td>{modules[1].noPracticals}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   );
 };
 
